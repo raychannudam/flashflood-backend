b14a4d54e05da496e8abc465b541298a0930d56b
channudam2002 <channudam.2002@gmail.com>
2024-10-28 00:23:04 +0700

    update cors

--- main.py
+++ main.py
@@ -24,7 +24,11 @@ async def lifespan(app: FastAPI):
 
 origins = [
     "http://127.0.0.1:8080",
+    "http://127.0.0.1",
     "http://localhost:8080",
+    "http://localhost",
+    "http://floodalert.live",
+    "https://floodalert.live"
 ]
 
 
